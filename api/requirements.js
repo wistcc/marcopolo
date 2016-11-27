2268c90917a0f1c4b068838402c010d0d8737fa6
wistcc <wistcc@hotmail.com>
2016-11-26 23:20:29 -0400

    Getting countries alpha3

--- api/requirements.js
+++ api/requirements.js
@@ -1,6 +1,7 @@
 import express from 'express';
 const router = express.Router();
 import request from 'request';
+import {lookup} from 'country-data';
 import Countries from '../data/countries';
 
 const wikiApiUrl = (title) => `https://en.wikipedia.org/w/api.php?action=query&titles=${title}&prop=revisions&rvprop=content&format=json`
@@ -11,7 +12,10 @@ const getCountriesWhichDontRequireVisa = body => {
   let countriesNoVisaRequired = [];
   while(match != null) {
       if (match[2] === 'Visa not required') {
-          countriesNoVisaRequired.push(match[1]);
+          let code = lookup.countries({name: match[1]})[0];
+          
+          if(code)
+          countriesNoVisaRequired.push(code.alpha3);
       }
       match = regex.exec(body);
   }
